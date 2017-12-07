60ab3bd47e09ca5f5ce69131be6aaff20e2e3743
Kevin <604327073@qq.com>
2017-12-07 19:41:13 +0800

    增加进度条插件并完成登陆页面

--- public/manage_my/js/login.js
+++ public/manage_my/js/login.js
@@ -1,4 +1,5 @@
 $(function(){
+    // 1.测试代码
     // $('button[type=submit]').click(function(event){
     //     event.preventDefault();
     //     $.ajax({
@@ -10,6 +11,16 @@ $(function(){
     //         }
     //     })
     // })
+
+     // 2.初始化 表单验证插件
+    /*
+        .on('success.form.bv')
+        这个事件是插件提供的自定义事件
+            当我们点击submit按钮
+                验证数据
+                    失败 提示用户 失败
+                验证成功
+    */
 
     //使用表单校验插件
     $('form').bootstrapValidator({
@@ -34,9 +45,12 @@ $(function(){
                     },
                     //长度校验
                     stringLength: {
-                        min: 6,
-                        max: 30,
+                        min: 3,
+                        max: 12,
                         message: '用户名长度必须在6到30之间'
+                    },
+                    callback: {
+                        message: '用户名不存在'
                     },
                     //正则校验
                     regexp: {
@@ -54,8 +68,11 @@ $(function(){
                     //长度校验
                     stringLength: {
                         min: 6,
-                        max: 30,
+                        max: 16,
                         message: '密码长度必须在6到30之间'
+                    },
+                    callback: {
+                        message: '密码错误'
                     },
                     //正则校验
                     regexp: {
@@ -69,15 +86,19 @@ $(function(){
     }).on('success.form.bv', function (e) {
         e.preventDefault();
         //使用ajax提交逻辑
+        console.log('你点我啦');
+        // 增加进度条开启代码
+        //开启进度条
+        NProgress.start();
         $.ajax({
-            url: "/employee/empolyeeLogin",
+            url: "/employee/employeeLogin",
             data: $('form').serialize(),
             type: 'post',
-            success: function () {
+            success: function (backData) {
                 console.log(backData);
                 // 正确
                 if (backData.success == true) {
-                    window.location = './index.html';
+                    window.location.href = './index.html';
                 }
                 else {
                     // 获取验证插件对象
@@ -86,15 +107,34 @@ $(function(){
                     if (backData.error == 1000) {
                         // console.log('用户名不存在');
                         // 使用表单校验实例可以调用一些常用的方法。
-                        validator.updataStatus('username', )
+                        validator.updateStatus('username', 'INVALID', 'callback');
+                    }
+                    else if (backData.error == 1001) {
+                        // console.log('密码错误');
+                        //使用表单校验实例可以调用一些常用的方法。
+                        validator.updateStatus('password', 'INVALID', 'callback');
                     }
                 }
+                setTimeout(function () {
+                    // 收起进度条
+                    NProgress.done();
+                },1000)
             }
         })
     });
+    // 3.为重置表单绑定点击事件
+    $('button[type=reset]').click(function () {
+        // 获取验证插件对象
+        var validator = $("form").data('bootstrapValidator');
+        // 获取表单验证实例
+        validator.resetForm();
+    })
+// 4.测试进度条
+// 关闭进度条
+setTimeout(function () {
 
-
-
+    
+},2000)
 })
 
 
