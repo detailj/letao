a063fb27f8687ccb85bf661c7cc188c81bf6c903
Kevin <604327073@qq.com>
2017-12-10 15:37:57 +0800

    商品样式初步完成

---
+++ manage_my/js/products.js
@@ -0,0 +1,36 @@
+$(function () {
+    // 0.定义变量
+    var myPageNum = 1;
+    var myPageSize = 5;
+
+    // 1页面打开 获取数据
+    function getData() {
+        $.ajax({
+            url: "/product/queryProductDetailList",
+            data: {
+                page: myPageNum,
+                pageSize: myPageSize,
+            },
+            success: function (backData) {
+                // console.log(bacKData);
+                // 渲染数据
+                $('tbody').html(template('products', backData));
+                // 初始化 分页控件
+                $(".pagination").bootstrapPaginator({
+                    bootstrapMajorVersion: 3, //默认是2，如果是bootstrap3版本，这个参数必填
+                    currentPage: myPageNum, //当前页
+                    totalPages: Math.ceil(backData.total / backData.size), //总页数
+                    size: "small", //设置控件的大小，mini, small, normal,large
+                    onPageClicked: function (event, originalEvent, type, page) {
+                        // 为按钮绑定点击事件 page:当前点击的按钮值
+                        myPageNum = page;
+                        // 重新获取数据
+                        getData();
+                    }
+                });
+            }
+        })
+    }
+    // 默认调用一次 getData()
+    getData();
+})
---
+++ public/manage_my/js/products.js
@@ -0,0 +1,6 @@
+$(function () {
+    // 0.定义变量
+    var myPageNum = 1;
+    var myPageSize = 5;
+    
+})
